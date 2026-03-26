096145042ad17f6969f5857a215c65902016a1d3
Njyhier <njerinjihia43@gmail.com>
2026-03-26 20:22:06 +0300

    added auth routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import type {Express, Request, Response} from 'express'
+import type { Express } from "express";
+import { PORT } from "./secrets.ts";
+import rootRouter from "./routes/mainRoute.ts";
 
-const app: Express = express()
+const app: Express = express();
+app.use(express.json());
+app.use("/", rootRouter);
 
-app.get('/', (req:Request, res:Response)=>{
-  res.send('Working')
-})
-
-app.listen(3000, ()=>{console.log('App Working')})
+app.listen(PORT, () => {
+  console.log("App Working");
+});
