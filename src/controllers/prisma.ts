ae232c1fc6fb740d1a441d4c4e73c1008ea67187
Njyhier <njerinjihia43@gmail.com>
2026-04-18 10:50:43 +0300

    refactor: comment out unused PrismaBetterSqlite3 adapter code in prisma.ts

--- src/controllers/prisma.ts
+++ src/controllers/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import dotenv from "dotenv";
 import { dot } from "node:test/reporters";
 
@@ -7,8 +7,8 @@ dotenv.config({ path: ".env" });
 
 const db_url = process.env.DATABASE_URL;
 
-const adapter = new PrismaBetterSqlite3({
-  url: db_url || "file:./clonex.db",
-});
+// const adapter = new PrismaBetterSqlite3({
+//   url: db_url || "file:./clonex.db",
+// });
 
-export const prisma = new PrismaClient({ adapter, log: ["query"] });
+export const prisma = new PrismaClient({ log: ["query"] });
