591df3a2f0c602a18b7be9b753ab9e27f51b442a
Njyhier <njerinjihia43@gmail.com>
2026-04-18 11:08:52 +0300

    refactor: comment out unused PrismaBetterSqlite3 adapter code and update package-lock.json dependencies

--- dist/controllers/prisma.js
+++ dist/controllers/prisma.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const client_1 = require("@prisma/client");
-const adapter_better_sqlite3_1 = require("@prisma/adapter-better-sqlite3");
+// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({ path: ".env" });
 const db_url = process.env.DATABASE_URL;
-const adapter = new adapter_better_sqlite3_1.PrismaBetterSqlite3({
-    url: db_url || "file:./clonex.db",
-});
-exports.prisma = new client_1.PrismaClient({ adapter, log: ["query"] });
+// const adapter = new PrismaBetterSqlite3({
+//   url: db_url || "file:./clonex.db",
+// });
+exports.prisma = new client_1.PrismaClient({ log: ["query"] });
