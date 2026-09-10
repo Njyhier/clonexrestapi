7061dd75480228af14eb3f80434334ca060ae031
Njyhier <njerinjihia43@gmail.com>
2026-09-10 13:39:32 +0300

    Fix PostgreSQL TLS certificate validation

--- src/controllers/prisma.ts
+++ src/controllers/prisma.ts
@@ -1,15 +1,28 @@
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import dotenv from "dotenv";
+
+// dotenv.config({ path: ".env" });
+
+// const db_url = process.env.DATABASE_URL;
+// if (!process.env.DATABASE_URL && process.env.NODE_ENV !== "production") {
+//   console.warn("DATABASE_URL not set (skipping strict validation)");
+// }
+// const adapter = new PrismaPg({
+//   connectionString: db_url,
+// });
+
+// export const prisma = new PrismaClient({ adapter, log: ["query"] });
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import dotenv from "dotenv";
-
 dotenv.config({ path: ".env" });
-
 const db_url = process.env.DATABASE_URL;
 if (!process.env.DATABASE_URL && process.env.NODE_ENV !== "production") {
   console.warn("DATABASE_URL not set (skipping strict validation)");
 }
 const adapter = new PrismaPg({
   connectionString: db_url,
+  ssl: { rejectUnauthorized: false },
 });
-
 export const prisma = new PrismaClient({ adapter, log: ["query"] });
